e3b9abeb637f7949f4fe6c2bc4f94c55dba874ea
Shaikh Vajib <shaikhvajib99@gmail.com>
2024-04-25 23:23:03 +0530

    25-04-2024 Updates

--- Vista/src/Pages/Home.jsx
+++ Vista/src/Pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="home">
+        <div className="hero-section min-h-screen bg-slate-500">
+          <div className="content-section">
+            <h1> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, consequatur.</h1>
+
+          </div>
+           
+
+        </div>
+
+
+
+
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
