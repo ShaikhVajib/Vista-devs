634593f33755a86d0d80d72d44db93bf1f8649b9
Aditya Panchal <adipanchal443@gmail.com>
2024-04-26 23:14:20 +0530

    Override Test

--- Vista/src/Pages/Home.jsx
+++ Vista/src/Pages/Home.jsx
@@ -8,15 +8,15 @@ function Home() {
     <>
       <div className="home">
         <div className="hero-section bg-black flex ">
-          <div className="content-section max-w-screen-xl mx-auto content-end flex flex-col gap-6 flex-nowrap justify-center">
-            <div className="content pb-16 flex flex-col gap-8">
-              <h1 className="text-white text-5xl text-center">Designing a BattleGround Today</h1>
-              <p className="text-gray-500  justify-center text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <div className="content-section max-w-screen-xl mx-auto flex flex-col gap-6 justify-center">
+            <div className="content pb-16 flex justify-center text-center flex-col items-center gap-5">
+              <h1>Designing a BattleGround Today</h1>
+              <p className="text-gray-500 w-2/3 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
-              <div className="con-btn flex flex-row-reverse justify-center gap-5">
-              <PrimaryButton btnHref="#contact" btnText="What We Do"/>
-              <SecondaryButton btnHref="#contact" btnText="View Works"/>
+              <div className="con-btn flex flex-col flex-col-reverse items-center sm:flex-row sm:flex-row-reverse justify-center gap-5">
+                <PrimaryButton btnHref="#contact" btnText="What We Do"/>
+                <SecondaryButton btnHref="#contact" btnText="View Works"/>
               </div>
             </div>
           </div>
