f89bed3c8309b98f0394689112e8f718cf87f206
Dipesh Nayka <dipeshnayka347@gmail.com>
2024-04-27 00:02:35 +0530

    pull

--- Vista/src/Components/Footer.jsx
+++ Vista/src/Components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../index.css';
+import './Footer.css';
 
 const Footer = () => {
   return (
    <>
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+<footer class="shadow">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
     </span>
