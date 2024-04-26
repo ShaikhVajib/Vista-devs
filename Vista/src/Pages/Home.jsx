8a6b9aa477703fad26643c8ce88daa7ad4c76491
Shaikh Vajib <shaikhvajib99@gmail.com>
2024-04-27 00:08:57 +0530

    change File

--- Vista/src/Pages/Home.jsx
+++ Vista/src/Pages/Home.jsx
@@ -4,8 +4,7 @@ import PrimaryButton from "../Components/PrimaryButton";
 import SecondaryButton from "../Components/SecondaryButton";
 import Cta from "../Components/Cta";
 
-const Home = () => {
-
+function Home() {
   return (
     <>
       <div className="home">
@@ -27,15 +26,6 @@ const Home = () => {
               <div className="con-btn flex flex-row-reverse justify-center gap-5 max-sm:flex-col-reverse max-sm:justify-center">
                 <PrimaryButton btnHref="#contact" btnText="What We Do" />
                 <SecondaryButton btnHref="#contact" btnText="View Works" />
-          <div className="content-section max-w-screen-xl mx-auto flex flex-col gap-6 justify-center">
-            <div className="content pb-16 flex justify-center text-center flex-col items-center gap-5">
-              <h1>Designing a BattleGround Today</h1>
-              <p className="text-gray-500 w-2/3 text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-              <div className="con-btn flex flex-col flex-col-reverse items-center sm:flex-row sm:flex-row-reverse justify-center gap-5">
-                <PrimaryButton btnHref="#contact" btnText="What We Do"/>
-                <SecondaryButton btnHref="#contact" btnText="View Works"/>
               </div>
             </div>
           </div>
@@ -66,7 +56,7 @@ const Home = () => {
         </div>
       </div>
     </>
-  )
-};
+  );
+}
 
-export default Home
+export default Home;
