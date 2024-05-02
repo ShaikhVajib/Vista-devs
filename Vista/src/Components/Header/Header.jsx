60c145b44fbfce43493137a049705e91c1306d7f
Aditya Panchal <adipanchal443@gmail.com>
2024-05-03 01:37:41 +0530

    Added Mobile Menu

--- Vista/src/Components/Header/Header.jsx
+++ Vista/src/Components/Header/Header.jsx
@@ -1,46 +1,113 @@
-import React from 'react';
-import '../../index.css';
-import './Header.css';
-import logo from '../../assets/logo.svg';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import "../../index.css";
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-
   return (
-    <>    
+    <>
       <nav className=" border-gray-200 sticky top-0">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src={logo} className="h-8 header-logo" alt="Logo" />
+        <div className="max-w-screen-xl py-6 flex flex-wrap items-center justify-between mx-auto p-4">
+          {/* Logo */}
+          <a
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img src={logo} className="h-8 z-50 header-logo" alt="Logo" />
           </a>
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              {/* <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-              </svg> */}
-          </button>
+          {/* Mobile Menu */}
+          <label
+            class="relative md:hidden z-40 cursor-pointer px-3 py-6"
+            for="mobile-menu"
+          >
+            <input class="peer hidden" type="checkbox" id="mobile-menu" />
+            <div class="relative z-50 block h-[1px] w-7 bg-black bg-transparent content-[''] before:absolute before:top-[-0.35rem] before:z-50 before:block before:h-full before:w-full before:bg-white before:transition-all before:duration-200 before:ease-out before:content-[''] after:absolute after:right-0 after:bottom-[-0.35rem] after:block after:h-full after:w-full after:bg-white after:transition-all after:duration-200 after:ease-out after:content-[''] peer-checked:bg-transparent before:peer-checked:top-0 before:peer-checked:w-full before:peer-checked:rotate-45 before:peer-checked:transform after:peer-checked:bottom-0 after:peer-checked:w-full after:peer-checked:-rotate-45 after:peer-checked:transform"></div>
+            <div class="fixed inset-0 z-40 hidden h-full w-full backdrop-blur-sm peer-checked:block">
+              &nbsp;
+            </div>
+            <div class="fixed top-0 right-0 z-40 w-full -translate-y-full overflow-y-auto overscroll-y-none transition duration-500 peer-checked:translate-y-0">
+              <div class="float-right min-h-full w-[100%] bg-black px-6 pt-24 pb-16 justify-center flex text-center shadow-2xl text-white">
+                <menu>
+                  <li>
+                    <NavLink
+                      to="/"
+                      className=" ${isActive ? '' : ''} block py-3 px-3 text-white rounded md:bg-transparent  md:p-0 "
+                      aria-current="page"
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/about"
+                      className="${isActive ? '' : ''} block py-3 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 "
+                    >
+                      About
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/services"
+                      className="${isActive ? '' : ''} block py-3 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 "
+                    >
+                      Services
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/contact"
+                      className="${isActive ? '' : ''} block py-3 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 "
+                    >
+                      Contact
+                    </NavLink>
+                  </li>
+                </menu>
+              </div>
+            </div>
+          </label>
+          {/* Desktop Nav Menu */}
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="main-menu font-medium flex flex-col p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
               <li>
-                <NavLink to="/" className=" ${isActive ? '' : ''} block py-2 px-3 text-white rounded md:bg-transparent  md:p-0 " aria-current="page">Home</NavLink>
+                <NavLink
+                  to="/"
+                  className=" ${isActive ? '' : ''} block py-2 px-3 text-white rounded md:bg-transparent  md:p-0 "
+                  aria-current="page"
+                >
+                  Home
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/about" className="${isActive ? '' : ''} block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 ">About</NavLink>
+                <NavLink
+                  to="/about"
+                  className="${isActive ? '' : ''} block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 "
+                >
+                  About
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/services" className="${isActive ? '' : ''} block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 ">Services</NavLink>
+                <NavLink
+                  to="/services"
+                  className="${isActive ? '' : ''} block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 "
+                >
+                  Services
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/contact" className="${isActive ? '' : ''} block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 ">Contact</NavLink>
+                <NavLink
+                  to="/contact"
+                  className="${isActive ? '' : ''} block py-2 px-3 rounded  md:hover:bg-transparent md:border-0 md:p-0 "
+                >
+                  Contact
+                </NavLink>
               </li>
             </ul>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Header
-
-
+export default Header;
